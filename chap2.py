d6a782d01cd9d71ba1c5c23779d60899e93086cd
wal <wwang.dev@gmail.com>
2018-03-10 21:40:47 -0800

    init

--- chap2.py
+++ chap2.py
@@ -16,6 +16,46 @@ last_name = "wang"
 full_name = first_name + " " + last_name
 print(full_name)
 
-favorite_language = 'python '
+favorite_language = ' python '
 print(favorite_language)
-favorite_language.rstrp()
+print(favorite_language.rstrip())
+print(favorite_language.strip())
+
+# 2-3
+name = "wal"
+print("Hello " + name + " python?")
+
+# 2-4
+name = "wal"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# 2-5
+msg = 'AL said "A person "'
+print(msg)
+
+# 2-6
+name = "AL"
+msg = '"A person "'
+print(name + " said " + msg)
+
+# 2-7
+name = "\tWal\n"
+print(name)
+print(name.lstrip())
+print(name.strip())
+print(name.rstrip())
+print(name)
+
+# 2-8
+print(5 + 3)
+print(2 * 4)
+print(12 - 4)
+print(64 / 8)
+
+# 2-9
+# testing of str()
+fav_num = 48
+msg = "my fav num is " + str(fav_num)
+print(msg)
