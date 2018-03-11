a43cff5bc294965416a6968905b1ff6b1fa771c9
wal <wwang.dev@gmail.com>
2018-03-10 21:16:31 -0800

    init

---
+++ chap2.py
@@ -0,0 +1,21 @@
+# 2-1
+message = "Hello Python"
+print(message)
+
+# 2-2
+message = "Hello World"
+print(message)
+
+name = "weichao wang"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "weichao"
+last_name = "wang"
+full_name = first_name + " " + last_name
+print(full_name)
+
+favorite_language = 'python '
+print(favorite_language)
+favorite_language.rstrp()
